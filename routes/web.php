5ead9613ce24981fb2e172348e406f73686137ee
xuan <zzdark.knightzz97@gmail.com>
2024-07-11 17:08:18 +0700

    create

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,5 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('hello world');
 });
+Route::get('/create', [UserController::class, 'create']);
+
